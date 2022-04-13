90b7813dad748ee77f4b53e3a4e4db10992860cb
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2022-04-12 23:59:29 -0300

    updated version

--- site-estatico/terraform/main.tf
+++ site-estatico/terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "0.14.8"
+  
 
   required_providers {
     aws = {
--- terraform-basico/02-alterando-destruindo/main.tf
+++ terraform-basico/02-alterando-destruindo/main.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = "0.14.4"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
